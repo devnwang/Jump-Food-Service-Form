8e2b06f72b86968986dfb4bca756f30bfea32bbb
nikkijwang <nikkijwang@gmail.com>
2021-08-14 01:48:42 -0700

    add aside for the tips option and show subtotal, tax, and tips

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-let total = 0;
+let subtotal = 0;
+let tax = 0;
+let tips = 0;
+let cartTotal = 0;
 let rowCount = 0;
+let itemList = [];
+
+const formatPrice = (price) => {
+    return (price).toLocaleString(undefined, { minimumFractionDigits : 2 });
+}
+
+const updatePriceDisplay = (id, price) => {
+    document.getElementById(id).innerHTML = "$" + formatPrice(price);
+}
+
+const updateItemQty = (event) => {
+    console.log(event);
+    
+    // retrieve the new quantity value
+    let newQty = event.target.value;
+    // grab the index of the item within the array
+    let selector = (event.target.id).split("_")[1] - 1;
+
+    let itemPrice = itemList[selector].price;
+
+    // remove item total price from the current subtotal
+    subtotal = subtotal - (itemList[selector].qty * itemPrice);
+    
+    // update the item list information
+    itemList[selector].qty = newQty;
+
+    let totalItemPrice = itemList[selector].qty * itemPrice;
+
+    // update subtotal
+    subtotal = subtotal + totalItemPrice;
+    updatePriceDisplay("subtotal", subtotal);
+
+    // update estimated tax
+    tax = subtotal * 0.05;
+    updatePriceDisplay("tax", tax);
+
+    // update cart total
+    cartTotal = subtotal + tax + tips;
+    updatePriceDisplay("total", cartTotal);
+}
 
 // add new item to the table
 const addNewItem = (item, price) => {
@@ -7,7 +50,7 @@ const addNewItem = (item, price) => {
     // TODO: Don't add any duplicate items to the list
 
     // format the price
-    let dspPrice = (price).toLocaleString(undefined, { minimumFractionDigits : 2 });
+    let dspPrice = formatPrice(price);
     
     // markup for the new table row
     let tblMarkup =
@@ -17,8 +60,14 @@ const addNewItem = (item, price) => {
             "<td>" + quantitySelect(++rowCount) + "</td>" +
         "</tr>";
 
+    // append item to the item list
+    itemList.push({row: rowCount, price: dspPrice, qty: 0});
+
     // append row to the table
     document.getElementById("item-table-body").innerHTML += tblMarkup;
+
+    // add event listener to the item's quantity selector
+    document.getElementById("sel_" + rowCount).addEventListener('change', updateItemQty);
 }
 
 // add event listener to "Add Item" button
@@ -44,6 +93,8 @@ const quantitySelect = (row) => {
     return selMarkup;
 }
 
+// TODO: Option to add tip
+// Add listeners to radio button to update the cart total based on the selected amount of tip
+
 // TODO: Calculate the cart total
 
-// TODO: Option to add tip
